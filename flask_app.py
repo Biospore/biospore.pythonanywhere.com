a7e8f9d717e32b019014c1fec1bfbb5399513b6d
biospore <biospore@hackerdom.ru>
2015-11-04 21:42:28 +0500

    Update 04/11/15

--- flask_app.py
+++ flask_app.py
@@ -1,11 +1,55 @@
 
-# A very simple Flask Hello World app for you to get started with...
 
-from flask import Flask
+from flask import Flask, render_template
+
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello from Flask!'
 
+@app.route('/')
+@app.route('/index/')
+def home():
+    return render_template('home.html')
+
+
+@app.route('/<page>/')
+def autofind(page='index'):
+    return render_template(page + '.html')
+
+
+@app.errorhandler(404)
+def not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(403)
+def forbidden(e):
+    return render_template('403.html'), 403
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
+@app.errorhandler(410)
+def gone(e):
+    return render_template('410.html'), 410
+
+@app.context_processor
+def get_all_menu_items():
+    items = [
+        ('/',           'Home'),
+        ('/gallery/',   'Gallery'),
+        ('/about/',     'About')
+    ]
+    return dict(menu_items=items)
+"""
+@app.context_processor
+def get_all_pages():
+    result = set()
+    for rule in app.url_map.iter_rules():
+        if "GET" in rule.methods:
+            url = rule.rule
+            result.add(url)
+    return dict(pages=result)
+"""
