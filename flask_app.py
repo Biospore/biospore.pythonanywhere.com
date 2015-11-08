2386db88e1c3cb093ffd14bc5a4611a88b12ed9d
biospore <biospore@hackerdom.ru>
2015-11-08 21:56:59 +0500

    Added some features.

--- flask_app.py
+++ flask_app.py
@@ -8,13 +8,29 @@ app = Flask(__name__)
 
 @app.route('/')
 @app.route('/index/')
+@app.route('/home/')
 def home():
     return render_template('home.html')
 
 
-@app.route('/<page>/')
-def autofind(page='index'):
-    return render_template(page + '.html')
+@app.route('/about/')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/gallery/')
+def gallery():
+    return render_template('gallery.html')
+
+
+@app.route('/barley-break/')
+def bb():
+    return render_template('barley-break.html')
+
+
+#@app.route('/<page>/')
+#def autofind(page='index'):
+#    return render_template(page + '.html')
 
 
 @app.errorhandler(404)
